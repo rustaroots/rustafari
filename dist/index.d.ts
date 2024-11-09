a32fb1c4109aa50f6b5ffcc03c36f36c24b0c7cc
rustaroots <rootsrustafari@gmail.com>
2024-11-09 13:08:31 +0100

    clean code

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "otechdo" {
-    class Otechdo {
-        constructor(element: HTMLElement);
-        addClass(className: string): void;
-        removeClass(className: string): void;
-        setStyle(property: string, value: string): void;
-        on(event: string, handler: (event: Event) => void): void;
-    }
-}
